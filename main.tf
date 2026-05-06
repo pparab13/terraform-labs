f2de596258b8ffa5597869de9105cc96907b253a
Prathamesh <prathameshparab05@gmail.com>
2026-05-06 12:45:27 +0530

    added locals

--- main.tf
+++ main.tf
@@ -1,21 +1,32 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~> 4.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
-}
+  }
 }
 provider "azurerm" {
   features {
-    
+
   }
 }
 
-resource "azurerm_resource_group" "contoso_rg" {
-   name     = "contoso_rg"
-   location = "UK South"
-  tags = { 
+locals {
+  prefix = "contoso"
+  region = "UK South"
+  tags = {
     cost_center = "contoso research"
-   }
- }
+  }
+}
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
+
+}
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
+}
