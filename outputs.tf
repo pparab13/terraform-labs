faf1147645ba7347b3399063908ec2a5272aee17
Prathamesh <prathameshparab05@gmail.com>
2026-05-11 19:22:59 +0530

    imported mgs and rgs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "contoso_rg_id" {
-  value = azurerm_resource_group.contoso_rg.id
-  description = "don't show actual data on cli output"
-  sensitive = true
-}
-output "contoso_dev_rg_id" {
-  value = azurerm_resource_group.contoso_dev_rg.id
+output "available_subscriptions" {
+  value = data.azurerm_subscriptions.available.subscriptions
 }
